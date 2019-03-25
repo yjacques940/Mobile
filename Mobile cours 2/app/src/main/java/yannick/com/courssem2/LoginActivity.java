48cfe31ca6f6f9b61d9316700df67a196920edc0
Yannick <yannick.jacques940@gmail.com>
2019-03-25 10:25:26 -0400

    Ajotuer le login firebase (ne pas oubluier le auth =...

--- Mobile cours 2/app/src/main/java/yannick/com/courssem2/LoginActivity.java
+++ Mobile cours 2/app/src/main/java/yannick/com/courssem2/LoginActivity.java
@@ -1,16 +1,26 @@
 package yannick.com.courssem2;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-
+FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        auth = FirebaseAuth.getInstance();
         setListener();
     }
 
@@ -30,5 +40,24 @@ public class LoginActivity extends AppCompatActivity {
         EditText email = findViewById(R.id.editText_emailLogin);
         EditText password = findViewById(R.id.editText_passwordLogin);
 
+        auth.signInWithEmailAndPassword(email.getText().toString(),password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    sendUserToMainActivity();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Error somewhere",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void sendUserToMainActivity()
+    {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
